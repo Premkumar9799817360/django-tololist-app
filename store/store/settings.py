4bd2e028fcb86921f7401e2da6c930f66e550cc3
Pro_coder <83695512+Premkumar9799817360@users.noreply.github.com>
2022-07-10 17:48:55 +0530

    Update settings.py

--- store/store/settings.py
+++ store/store/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a@tcc3214b*1b9ki4)72&e5%s6icp$2p5gvip3mur4s$1z$(zm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["prem-django-todolist-app.harokuapp.com","localhost"]
 
 
 # Application definition
